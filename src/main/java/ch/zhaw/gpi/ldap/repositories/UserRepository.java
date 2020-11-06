5bda9afdd8d24c01e9108e22524cbd55ca936d7a
Björn Scheppler <scep@zhaw.ch>
2020-11-06 11:09:50 +0100

    Bis und mit Kapitel 8.4

--- src/main/java/ch/zhaw/gpi/ldap/repositories/UserRepository.java
+++ src/main/java/ch/zhaw/gpi/ldap/repositories/UserRepository.java
@@ -1,9 +1,14 @@
 package ch.zhaw.gpi.ldap.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import ch.zhaw.gpi.ldap.entities.User;
 
 public interface UserRepository extends JpaRepository<User, String> {
-    
+
+
+   
 }
